ca52998e5b4361f3e1048ebf39f59df7a00d6c7a
Alija Bobija <bobija.alija@gmail.com>
2019-06-21 17:10:23 +0200

    Improved way of monitoring ws live status

--- src/broker.js
+++ src/broker.js
@@ -1,33 +1,46 @@
 const ws = require('ws')
 
+const lifetimeThreshold = 10 // sec
+
 class Broker {
     constructor(server) {
         let self = this
         this.wss = new ws.Server({ server })
         
         this.wss.on('connection', ws => {
-            console.log('ws client connected')
+            console.log('ws client connected', ws)
             
-            ws.isAlive = true
-            ws.on('pong', () => ws.isAlive = true)
+            ws.refreshLifetime = () => ws.lifetime = lifetimeThreshold
+            ws.isAlive = () => ws.lifetime >= 0
+
+            ws.refreshLifetime()
+
+            ws.on('pong', ws.refreshLifetime)
 
             ws.on('message', msg => {
+                ws.refreshLifetime()
+
                 console.log('received from ws client: ', msg)
                 ws.send(msg)
             })
         })
 
+        let lifeTimePingSendSecond = Math.ceil(lifetimeThreshold / 3)
+
         setInterval(() => {
             self.wss.clients.forEach(ws => {
-                if(ws.isAlive === false) {
+                ws.lifetime--
+
+                if(! ws.isAlive()) {
                     console.log('ws client disconnected')
                     return ws.terminate()
                 }
                 
-                ws.isAlive = false
-                ws.ping(() => {});
+                if(ws.lifetime == lifeTimePingSendSecond) {
+                    ws.ping(() => {})
+                }
             })
-        }, 30000)
+        }, 1000)
     }
 }
 
