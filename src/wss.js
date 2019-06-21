965c5b1e10023a66ce0e20e02cc7fdf05b7f9807
Alija Bobija <bobija.alija@gmail.com>
2019-06-21 11:59:56 +0200

    Ping/Pong implemented. Monitoring client activity

--- src/wss.js
+++ src/wss.js
@@ -3,13 +3,28 @@ module.exports = function(server) {
     let wss = new ws.Server({ server })
 
     wss.on('connection', ws => {
-        console.log('ws client connected');
+        console.log('ws client connected')
+
+        ws.isAlive = true
+        ws.on('pong', () => ws.isAlive = true)
     
         ws.on('message', msg => {
-            console.log('received from ws client: ', msg);
-            ws.send(msg);
-        });
-    });
+            console.log('received from ws client: ', msg)
+            ws.send(msg)
+        })
+    })
+
+    setInterval(() => {
+        wss.clients.forEach(ws => {
+            if(ws.isAlive === false) {
+                console.log('ws client disconnected')
+                return ws.terminate()
+            }
+            
+            ws.isAlive = false
+            ws.ping(() => {});
+        })
+    }, 30000)
 
     return wss
 }
