c963e1acb1e40d5a652de6b30fad7a518020c8ef
Alija Bobija <bobija.alija@gmail.com>
2019-06-21 11:04:44 +0200

    Use ws package instead of express-ws

--- src/app.js
+++ src/app.js
@@ -1,13 +1,25 @@
-const app = require('express')();
-const appWs = require('express-ws')(app);
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app)
+const WebSocket = require('ws')
 
-app.ws('/', ws => {
-    console.log('ws client connected.');
+const wss = new WebSocket.Server({ server: server });
+
+wss.on('connection', ws => {
+    console.log('ws client connected');
 
     ws.on('message', msg => {
-        console.log('ws received message: ', msg);
+        console.log('received from ws client: ', msg);
         ws.send(msg);
     });
 });
 
-app.listen(8080, () => console.log('Server has been started.'));
+var api = express.Router()
+
+api.get('/dev', (req, res) => {
+    res.json({ count: wss.clients.size  });
+})
+
+app.use('/api', api)
+
+server.listen(8080, () => console.log('Server has been started.'))
