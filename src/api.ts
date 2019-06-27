29fda11d64f6a94fd32be8f6047f455373618581
Alija Bobija <bobija.alija@gmail.com>
2019-06-27 21:00:03 +0200

    Return router in chained way

--- src/api.ts
+++ src/api.ts
@@ -2,11 +2,8 @@ import express from 'express'
 import Broker from './broker'
 
 export default function api(broker: Broker): express.Router {
-    let router = express.Router()
-
-    router.get('/channels', (req, res) => {
-        res.json(broker.getChannels())
-    })
-    
-    return router
+    return express.Router()
+        .get('/channels', (req, res) => {
+            res.json(broker.getChannels())
+        })
 }
