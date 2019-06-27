70e0c7aab5433dfca6272c029f948e7d817fe270
Alija Bobija <bobija.alija@gmail.com>
2019-06-27 19:54:19 +0200

    /api/dev route modified

--- src/api.ts
+++ src/api.ts
@@ -4,9 +4,9 @@ import Broker from './broker'
 export default function api(broker: Broker): express.Router {
     let router = express.Router()
 
-    /*router.get('/dev', (req, res) => {
-        res.json({ count: broker.subscribers.length  });
-    })*/
+    router.get('/dev', (req, res) => {
+        res.json({ this_is_hardcoded: true });
+    })
     
     return router
 }
