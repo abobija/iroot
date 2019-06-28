415444813c3a245da7bf88f80eae5d540ace5b72
Alija Bobija <bobija.alija@gmail.com>
2019-06-28 16:33:00 +0200

    Dashboard folder

--- src/app.ts
+++ src/app.ts
@@ -2,17 +2,18 @@ import express from 'express'
 import http from 'http'
 import Broker from './model/broker'
 import api from './route/api'
+import path from 'path'
 import { jsonIgnoreReplacer } from 'json-ignore'
 
 const app = express()
 const server = http.createServer(app)
 const broker = new Broker(server)
 
-app.use('/api', api(broker))
-
 app.disable('etag')
-
 app.set('json replacer', jsonIgnoreReplacer)
 app.set('json spaces', 2)
+
+app.use('/dashboard', express.static(path.join(__dirname, '../dashboard')))
+app.use('/api', api(broker))
 
 server.listen(8080, () => console.log('Server has been started.'))
