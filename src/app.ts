e6b30df1fe042c03583b0a4ed19227960132b7b6
Alija Bobija <bobija.alija@gmail.com>
2019-06-29 09:55:43 +0200

    Dashboard css & js files

--- src/app.ts
+++ src/app.ts
@@ -14,8 +14,9 @@ app.set('json replacer', jsonIgnoreReplacer)
 app.set('json spaces', 2)
 
 app.use('/api', api(broker))
-app.use('/dashboard', express.static(path.join(__dirname, '../src/dashboard')))
-app.use('/static/bootstrap', express.static(path.join(__dirname, '../node_modules/bootstrap/dist')))
-app.use('/static/jquery', express.static(path.join(__dirname, '../node_modules/jquery/dist')))
+app.get('/dashboard', (_req, res) => res.sendFile(path.join(__dirname, '../static/dashboard.html')))
+app.use('/static', express.static(path.join(__dirname, '../static')))
+app.use('/lib/bootstrap', express.static(path.join(__dirname, '../node_modules/bootstrap/dist')))
+app.use('/lib/jquery', express.static(path.join(__dirname, '../node_modules/jquery/dist')))
 
 server.listen(8080, () => console.log('Server has been started.'))
