7ef7f11bf162c994755a3cef7fff7f2ad858da44
Alija Bobija <bobija.alija@gmail.com>
2019-06-29 09:41:31 +0200

    Dashboard folder moved inside of src

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ app.set('json replacer', jsonIgnoreReplacer)
 app.set('json spaces', 2)
 
 app.use('/api', api(broker))
-app.use('/dashboard', express.static(path.join(__dirname, '../dashboard')))
+app.use('/dashboard', express.static(path.join(__dirname, '../src/dashboard')))
 app.use('/static/bootstrap', express.static(path.join(__dirname, '../node_modules/bootstrap/dist')))
 app.use('/static/jquery', express.static(path.join(__dirname, '../node_modules/jquery/dist')))
 
